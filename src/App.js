ef0935eb65a74b8f2c94e0f57d28e86cb070e982
Kadyn Aaron <kadynaaron@Jonathans-MacBook-Pro-2.local>
2019-02-04 18:31:29 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React  from 'react';
+import './App.scss';
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="clip-text px-title">Pokemon</div>
+    </div>
+  );
+};
+
+export default App;
