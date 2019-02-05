e66427f1b91410afdcb45a730d820d05700a8bbd
Kadyn Aaron <kadynaaron@Jonathans-MacBook-Pro-2.local>
2019-02-05 07:27:30 -0700

    add Charizard and Blastoise

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ const App = () => {
   return (
     <div className="App">
       <div className="clip-text px-title">Pokemon</div>
+      <div className="experimental-string1">Charizard</div>
+      <div className="experimental-string2">Blastoise</div>
     </div>
   );
 };
